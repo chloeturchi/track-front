dd39aa2d24f07802d04082e9d1e7f8d921ba9093
Chloe <chloeturchi@gmail.com>
2020-10-09 15:08:25 +0200

    api request

---
+++ src/api/tracker.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://8bdccd0f84be.ngrok.io'
+});
+
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  console.log(state)
   return (
-    <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input label="Email" />
-      <Spacer />
-      <Input label="Password" />
-      <Spacer>
-        <Button title="Sign Up" />
-      </Spacer>
-    </View>
+    <KeyboardAvoidingView>
+      <ScrollView contentContainerStyle={styles.contentContainerStyle}>
+        <View style={styles.container}>
+          <Spacer>
+            <Text h3>Sign Up for Tracker</Text>
+          </Spacer>
+          <Input 
+            label="Email" 
+            value={email} 
+            onChangeText={setEmail} 
+            autoCapitalize="none"
+            autoCorrect={false} 
+          />
+          <Spacer />
+          <Input 
+            secureTextEntry
+            label="Password" 
+            value={password} 
+            onChangeText={setPassword} 
+            autoCapitalize="none"
+            autoCorrect={false} 
+          />
+          {state.errorMessage ? (
+            <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+          ) : null}
+          <Spacer>
+            <Button 
+              title="Sign Up"
+              onPress={() => signup({ email, password })} />
+          </Spacer>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
   );
 };
 
@@ -30,6 +58,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 200
+  },
+  contentContainerStyle: {
+    paddingVertical: 150,
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15
   }
 });
 
