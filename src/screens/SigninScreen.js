9208fccec22fc549d8da3389135e1a58450bf660
Chloe <chloeturchi@gmail.com>
2020-10-09 13:16:20 +0200

    :sparkles: working navigation

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+const SigninScreen = ({ navigation }) => {
+  return <>
+      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+      <Button 
+        title="Go to Signup" 
+        onPress={() => navigation.navigate('Signup')}
+    />
+  </>
 };
 
 const styles = StyleSheet.create({});
